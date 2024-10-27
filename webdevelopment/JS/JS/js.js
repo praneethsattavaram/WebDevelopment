3db82201c8b09fc9ad51a038a2566c4da3401570
PRANEETH REDDY <108972019+praneethsattavaram@users.noreply.github.com>
2024-10-27 14:33:52 +0530

    Delete webdevelopment directory

--- webdevelopment/JS/JS/js.js
+++
@@ -1,8 +0,0 @@
-let fun1 = () => {
-    let red = Math.random()*255
-    let green = Math.random()*255
-    let blue = Math.random()*255
-    let finalColorValue = `rgb(${red}, ${green}, ${blue})`
-    let body = document.body
-    body.style.background = finalColorValue
-}
